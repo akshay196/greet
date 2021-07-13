4742e7811473e22591d84c74339e65c8543b9a4a
Akshay Gaikwad <akgaikwad001@gmail.com>
2021-07-13 16:57:33 +0530

    Initial commit

---
+++ greet_client/client.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/akshay196/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure()) // Default grpc run with SSL conneciton
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created clinet: %v", c)
+}
